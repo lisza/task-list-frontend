8adae2d695e22998dcbb66f1b7bc22a2f2109711
lisza <liszalisza@gmail.com>
2019-10-27 18:21:33 -0700

    Fix indentation

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,67 +3,67 @@ import data from '../data/tasks.json';
 
 
 class Tasks extends React.Component {
-	state = {
-		tasks: data,
-		itemsToShow: 5,
-		expanding: true
-	};
-	
-	showMore = () => {
-		this.setState(prevState => {
-			if (prevState.itemsToShow + 5 >= prevState.tasks.length) {
-				return { itemsToShow: prevState.tasks.length, expanding: false };
-			} else {
-				return { itemsToShow: prevState.itemsToShow + 5 };
-			}
-		});
-	}
+  state = {
+    tasks: data,
+    itemsToShow: 5,
+    expanding: true
+  };
+  
+  showMore = () => {
+    this.setState(prevState => {
+      if (prevState.itemsToShow + 5 >= prevState.tasks.length) {
+        return { itemsToShow: prevState.tasks.length, expanding: false };
+      } else {
+        return { itemsToShow: prevState.itemsToShow + 5 };
+      }
+    });
+  }
 
-	showLess = () => {
-		this.setState(prevState => {
-			if (prevState.itemsToShow - 5 <= 5) {
-				return { itemsToShow: 5, expanding: true };
-			} else {
-				return { itemsToShow: prevState.itemsToShow - 5 };
-			}
-		});
-	}
+  showLess = () => {
+    this.setState(prevState => {
+      if (prevState.itemsToShow - 5 <= 5) {
+        return { itemsToShow: 5, expanding: true };
+      } else {
+        return { itemsToShow: prevState.itemsToShow - 5 };
+      }
+    });
+  }
 
-	toggleCheckbox = (e) => {
-		const index = this.state.tasks.slice(0, this.state.itemsToShow).findIndex(task => {
-			return task.id === Number(e.target.id);
-		})
-		const tasks = [...this.state.tasks];
-		tasks[index]['completed'] = !tasks[index]['completed'];
+  toggleCheckbox = (e) => {
+    const index = this.state.tasks.slice(0, this.state.itemsToShow).findIndex(task => {
+      return task.id === Number(e.target.id);
+    })
+    const tasks = [...this.state.tasks];
+    tasks[index]['completed'] = !tasks[index]['completed'];
 
-		this.setState({ tasks: tasks });
-	}
+    this.setState({ tasks: tasks });
+  }
 
 
-	render() {
-		return (
-			<React.Fragment>
-				{this.state.tasks.slice(0, this.state.itemsToShow).map(task => {
-					return <div key={task.id}>
-						<label>
-							<input
-								id={task.id}
-								type='checkbox'
-								checked={task.completed}
-								onChange={this.toggleCheckbox}
-							/>
-							{task.details}
-						</label>
-					</div>
-				})}
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.tasks.slice(0, this.state.itemsToShow).map(task => {
+          return <div key={task.id}>
+            <label>
+              <input
+                id={task.id}
+                type='checkbox'
+                checked={task.completed}
+                onChange={this.toggleCheckbox}
+              />
+              {task.details}
+            </label>
+          </div>
+        })}
 
-				<button onClick={this.state.expanding ? this.showMore : this.showLess}>
-					{this.state.expanding ? 'Show More' : 'Show Less'}
-				</button>
+        <button onClick={this.state.expanding ? this.showMore : this.showLess}>
+          {this.state.expanding ? 'Show More' : 'Show Less'}
+        </button>
 
-			</React.Fragment>
-		);
-	}
+      </React.Fragment>
+    );
+  }
 }
 
 export default Tasks;
