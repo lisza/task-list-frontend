794ead193f0e7c87f3384040123b901b55447002
lisza <liszalisza@gmail.com>
2019-10-27 18:28:44 -0700

    Updates

---
+++ precache-manifest.a38a112c9ae028ed2e8fe70386033d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b3389cb39b16d8228c60c11977793e",
+    "url": "/task-list-frontend/index.html"
+  },
+  {
+    "revision": "66dfbfc910eb77efc2ec",
+    "url": "/task-list-frontend/static/css/main.658e71a8.chunk.css"
+  },
+  {
+    "revision": "94d205ff1e4b69933130",
+    "url": "/task-list-frontend/static/js/2.cb3f8ba5.chunk.js"
+  },
+  {
+    "revision": "66dfbfc910eb77efc2ec",
+    "url": "/task-list-frontend/static/js/main.15e5d302.chunk.js"
+  },
+  {
+    "revision": "433ba79872269518638e",
+    "url": "/task-list-frontend/static/js/runtime-main.51076573.js"
+  }
+]);
